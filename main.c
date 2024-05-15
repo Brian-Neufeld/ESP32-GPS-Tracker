58c9d9293bc61fdd5473811beeb5e2d388c8e22e
Brian Neufeld <36906721+Brian-Neufeld@users.noreply.github.com>
2024-05-14 21:40:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,81 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 #include <inttypes.h>
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
-#include <esp_err.h>
+
+#define BUF_SIZE (1024)
 
 
-const uart_port_t uart_num = UART_NUM_1;
 
-
-// Configure UART parameters
-
-/*
-void delay(int number_of_seconds)
+static void uart_task()
 {
-    // Converting time into milli_seconds
-    int milli_seconds = 1000 * number_of_seconds;
- 
-    // Storing start time
-    clock_t start_time = clock();
- 
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds)
-        ;
-}*/
-
-/*
-int main()
-{
-    const uart_port_t uart_num = UART_NUM_1;
-    uint8_t data[128];
-    int length = 0;
-    ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t*)&length));
-    length = uart_read_bytes(uart_num, data, length, 100);
-    printf("%d", length);
-    printf("here");
-
-}*/
-
-void app_main()
-{
-    printf("Starting Up... \n");
-    // vTaskDelay(100);
-    printf("hello world");
-    
-
-    uart_config_t uart_config = {
-    .baud_rate = 115200,
+    uart_config_t uart_config = 
+    {
+    .baud_rate = 9600,
     .data_bits = UART_DATA_8_BITS,
     .parity = UART_PARITY_DISABLE,
     .stop_bits = UART_STOP_BITS_1,
     .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    .source_clk = UART_SCLK_APB,
     };
-    
-    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 10, 9, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    // Setup UART buffered IO with event queue
-    const int uart_buffer_size = (1024 * 2);
-    QueueHandle_t uart_queue;
-    // Install UART driver using an event queue here
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, uart_buffer_size, \
-                                            uart_buffer_size, 10, &uart_queue, 0));
 
-    uint8_t data[128];
-    int length = 0;
-    ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t*)&length));
-    length = uart_read_bytes(uart_num, data, length, 100);
+    uart_driver_install(UART_NUM_1, BUF_SIZE, BUF_SIZE, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, 2, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
-    /*
+    uint8_t *data = (uint8_t*)malloc(BUF_SIZE);
+
     while (1)
     {
-        main();
+        const int len = uart_read_bytes(UART_NUM_1, data, BUF_SIZE, 10);
+        if (len>0)
+        {
+            data[len] = 0;
+            uart_write_bytes(UART_NUM_1, (const char *)data, len);
+            printf("%s\n", (const char *)data);
+            vTaskDelay(100);
+        }
     }
-    */
+
+    uart_flush(UART_NUM_1);
+
 }
 
+void app_main(void)
+{
+    uart_task();
+}
